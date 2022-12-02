09f7fb8a8dfff49b99dec2305c1a6496136430af
Topi Talvitie <topi.talvitie@iki.fi>
2022-12-02 21:00:20 +0200

    Make asmtohex.sh use stdin if no arguments given

--- asmtohex.sh
+++ asmtohex.sh
@@ -2,7 +2,14 @@
 
 TMPDIR="$(mktemp -d)"
 echo "BITS 32" > "${TMPDIR}/src.asm"
-echo "$@" >> "${TMPDIR}/src.asm"
+
+if [ "$#" == "0" ]
+then
+    cat >> "${TMPDIR}/src.asm"
+else
+    echo "$@" >> "${TMPDIR}/src.asm"
+fi
+
 if nasm -f bin -o "${TMPDIR}/binary" "${TMPDIR}/src.asm"
 then
     xxd -p "${TMPDIR}/binary"
